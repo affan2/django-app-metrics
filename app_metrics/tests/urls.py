609c3f66d5993d1605d9ef7301a134a8cf398fcd
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-09-15 15:33:17 +0400

    Fix bugs in the urlconf, part 1

--- app_metrics/tests/urls.py
+++ app_metrics/tests/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model
 # admin.autodiscover()
 #
 # urlpatterns = [
-#         '',
 #         re_path(r'^admin/', include(admin.site.urls)),
 #         re_path(r'^admin/metrics/', include('app_metrics.urls')),
 # ]
--- app_metrics/urls.py
+++ app_metrics/urls.py
@@ -3,7 +3,6 @@ from django.urls import re_path
 from app_metrics.views import *
 
 urlpatterns = [
-    '',
     re_path(
         regex   = r'^reports/$',
         view    = metric_report_view,
