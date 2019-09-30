003bb3de761016255d7017fd807e2ac6c8a3db50
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-09-30 19:29:45 +0400

    Fix back-end autocompletion

--- app_metrics/admin.py
+++ app_metrics/admin.py
@@ -1,22 +1,46 @@
 from django.contrib import admin
 
-from app_metrics.models import (Metric, MetricSet, MetricItem, MetricDay,
-                                MetricWeek, MetricMonth, MetricYear
-                                )
+from app_metrics.models import (
+    Metric, MetricSet, MetricItem, MetricDay,
+    MetricWeek, MetricMonth, MetricYear
+)
 
 
 class MetricAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'slug', 'num')
-    list_filter = ['metric__name']
+    list_display = ('__str__', 'slug', )
+    search_fields = ['name', 'slug', ]
 
     def slug(self, obj):
         return obj.metric.slug
 
 
-admin.site.register(Metric)
-admin.site.register(MetricSet)
-admin.site.register(MetricDay, MetricAdmin)
-admin.site.register(MetricWeek, MetricAdmin)
-admin.site.register(MetricMonth, MetricAdmin)
-admin.site.register(MetricYear, MetricAdmin)
-admin.site.register(MetricItem, MetricAdmin)
+admin.site.register(Metric, MetricAdmin)
+
+
+class MetricItemAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user', 'metric', ]
+
+
+admin.site.register(MetricItem, MetricItemAdmin)
+
+
+class MetricPeriodAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'slug', 'num')
+    list_filter = ['metric__name']
+    autocomplete_fields = ['metric', ]
+
+    def slug(self, obj):
+        return obj.metric.slug
+
+
+admin.site.register(MetricDay, MetricPeriodAdmin)
+admin.site.register(MetricWeek, MetricPeriodAdmin)
+admin.site.register(MetricMonth, MetricPeriodAdmin)
+admin.site.register(MetricYear, MetricPeriodAdmin)
+
+
+class MetricSetAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['metrics', 'email_recipients', ]
+
+
+admin.site.register(MetricSet, MetricSetAdmin)
